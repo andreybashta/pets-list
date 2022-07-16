32647ce01018eb9a07a331ba4177f26a65ed9e06
Andriy Bashta <a.bashta@relevant.software>
2022-07-16 14:55:45 +0300

    Added basic project, colors and icon

---
+++ petslist/petslistApp.swift
@@ -0,0 +1,17 @@
+//
+//  petslistApp.swift
+//  petslist
+//
+//  Created by Bashta on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct petslistApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ petslistTests/petslistTests.swift
@@ -0,0 +1,12 @@
+//
+//  petslistTests.swift
+//  petslistTests
+//
+//  Created by Bashta on 16.07.2022.
+//
+
+import XCTest
+@testable import petslist
+
+class petslistTests: XCTestCase {
+}
